2303d74af36618a69bc0cc262798b96a0f049e8f
Kumar <pai.kumar.taiwan@gamil.com>
2021-11-10 14:04:20 +0800

    feat: Setup cros for api route

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from endpoints.order import Order
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 api = Api(app)
 api.prefix = '/api'
 api.add_resource(Order, '/order', '/order/<string:uuid>')
